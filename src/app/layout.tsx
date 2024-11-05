3b55aaba1d9c7160447060b5b326ace848b0f89f
Nicolas SANS <nicolas.sansd@gmail.com>
2024-11-05 23:28:46 +0100

    change portfolio metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,19 @@ import {getLocale, getMessages} from "next-intl/server";
 const inter = Prompt({ subsets: ["latin"], weight: ["100", "200", '300', '400', '500', '600', '700'], variable: '--font-prompt' });
 
 export const metadata: Metadata = {
-  title: "SANS Nicolas - Portfolio",
-  description: "",
+    title: "SANS Nicolas - Portfolio",
+    description: "Développeur fullstack, mobile et Salesforce et DevOps spécialisé en React, Rust et NestJS",
     icons: {
       icon: '/favicon.ico'
+    },
+    metadataBase: new URL('https://nikho.dev'),
+    keywords: ['Nikho', 'Portfolio SANS Nicolas', 'Nantes', 'Développeur fullstack', 'mobile', 'Salesforce', 'React', 'Rust', 'DevOps'],
+    openGraph: {
+        title: "SANS Nicolas - Portfolio",
+        description: "Développeur fullstack, mobile et Salesforce spécialisé en React, Rust et DevOps",
+        type: 'website',
+        images: ['/pp.jpg'],
+        siteName: 'SANS Nicolas - Portfolio',
     },
 };
 
@@ -22,11 +31,11 @@ export default async function RootLayout({
 
   return (
       <html lang={await getLocale()}>
-          <body className={`${inter.className} bg-white-pastel dark:bg-dark`}>
-              <NextIntlClientProvider messages={await getMessages()}>
-                  {children}
-              </NextIntlClientProvider>
-          </body>
+      <body className={`${inter.className} bg-white-pastel dark:bg-dark`}>
+      <NextIntlClientProvider messages={await getMessages()}>
+          {children}
+      </NextIntlClientProvider>
+      </body>
       </html>
   );
 }
