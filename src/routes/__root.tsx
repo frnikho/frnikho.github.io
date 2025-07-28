f6f957541478015dd76e3523f5411e34d4e8c4e7
Nicolas SANS <nicolas.sansd@gmail.com>
2025-07-28 14:50:52 +0200

    feat: import app.css for styling and remove redundant stylesheet link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,6 +8,8 @@ import {
 } from '@tanstack/react-router'
 import type { ReactNode } from 'react'
 
+import '../styles/app.css';
+
 export const Route = createRootRoute({
     head: () => ({
         meta: [
@@ -71,7 +73,6 @@ export const Route = createRootRoute({
             }
         ],
         links: [
-            { rel: 'stylesheet', href: appCss },
             { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
             { rel: 'preconnect', href: "https://fonts.gstatic.com", crossOrigin: 'anonymous'},
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap'},
